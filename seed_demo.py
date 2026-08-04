9029af832617fe8a8e2825f10074ac474a706cf8
Priyanshu Soni <priyanshume4u@gmail.com>
2026-08-05 00:33:33 +0530

    Update seed_demo.py

--- seed_demo.py
+++ seed_demo.py
@@ -1,12 +1,12 @@
 from app import create_app, get_db
 
 SAMPLE_STUDENTS = [
-    ("ADM001", "Aarav Kumar", "6", "A", "1", "9000000001", "STU-DEMO-0001"),
-    ("ADM002", "Ananya Singh", "6", "A", "2", "9000000002", "STU-DEMO-0002"),
-    ("ADM003", "Deepak Yadav", "6", "A", "3", "9000000003", "STU-DEMO-0003"),
-    ("ADM004", "Kavya Verma", "6", "A", "4", "9000000004", "STU-DEMO-0004"),
-    ("ADM005", "Rohan Pal", "6", "A", "5", "9000000005", "STU-DEMO-0005"),
-    ("ADM006", "Sakshi Patel", "6", "A", "6", "9000000006", "STU-DEMO-0006"),
+    ("MPEC001", "Aarav Kumar", "6", "A", "1", "9000000001", "STU-DEMO-0001"),
+    ("MPEC002", "Ananya Singh", "6", "A", "2", "9000000002", "STU-DEMO-0002"),
+    ("MPEC003", "Deepak Yadav", "6", "A", "3", "9000000003", "STU-DEMO-0003"),
+    ("MPEC004", "Kavya Verma", "6", "A", "4", "9000000004", "STU-DEMO-0004"),
+    ("MPEC005", "Rohan Pal", "6", "A", "5", "9000000005", "STU-DEMO-0005"),
+    ("MPEC006", "Nisha Bakshi", "6", "A", "6", "9000000006", "STU-DEMO-0006"),
 ]
 
 app = create_app()
